dda2535660176bcd0d7d845cb9d98eb9b26b5bc6
ejaz <ejaz@ejazs-MacBook-Pro.local>
2021-08-09 17:42:28 +0500

    first commit

---
+++ lib/src/model/location_result.dart
@@ -0,0 +1,27 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+/// The result returned after completing location selection.
+class LocationResult {
+  /// The human readable name of the location. This is primarily the
+  /// name of the road. But in cases where the place was selected from Nearby
+  /// places list, we use the <b>name</b> provided on the list item.
+  String address; // or road
+
+  /// Google Maps place ID
+  String placeId;
+
+  String placeName = '';
+  String countryName = '';
+  String cityName = '';
+  String stateName = '';
+
+  /// Latitude/Longitude of the selected location.
+  LatLng latLng;
+
+  LocationResult({this.latLng, this.address, this.placeId, this.placeName, this.countryName, this.cityName, this.stateName});
+
+  @override
+  String toString() {
+    return 'LocationResult{address: $address, latLng: $latLng, placeId: $placeId, placeName: $placeName, countryName: $countryName, cityName: $cityName, stateName: $stateName}';
+  }
+}
